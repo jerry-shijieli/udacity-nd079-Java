314724f6f971d1a81f204fa15665f10ea79e1465
Jerry <sli41@ncsu.edu>
2022-04-14 22:26:36 -0700

    Update ExpirationChecker.java

--- cd0381/DependencyInjection/solutions/ExpirationChecker.java
+++ cd0381/DependencyInjection/solutions/ExpirationChecker.java
@@ -9,12 +9,13 @@ import java.util.stream.Collectors;
 
 public final class ExpirationChecker {
 
+  private final Clock clock;
   private final MetadataFetcher metadataFetcher;
 
-  // TODO: Mark this constructor with @javax.inject.Inject and give it parameters for the clock and
-  //       MetadataFetcher, so that those parameters can be injected by Guice.
-  ExpirationChecker() {
-    metadataFetcher = new MetadataFetcherImpl();
+  @Inject
+  ExpirationChecker(Clock clock, MetadataFetcher metadataFetcher) {
+    this.clock = clock;
+    this.metadataFetcher = metadataFetcher;
   }
 
   public List<Path> getExpiredFiles(List<Path> paths, Duration expiration) {
@@ -24,9 +25,7 @@ public final class ExpirationChecker {
   }
 
   private boolean isExpired(Path path, Duration expiration) {
-    // TODO: Update this code to get the current time from a Clock instance instead of
-    //       Instant.now().
-    Instant now = Instant.now();
+    Instant now = clock.instant();
     try {
       Instant modifiedTime = metadataFetcher.getLastModifiedTime(path);
       return now.isAfter(modifiedTime.plus(expiration));
