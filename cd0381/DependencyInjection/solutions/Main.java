20c53a8dc07c6dd5aeb4fc6a4a7fc4ecd2e7139f
Jerry <sli41@ncsu.edu>
2022-04-14 22:27:49 -0700

    Update Main.java

--- cd0381/DependencyInjection/solutions/Main.java
+++ cd0381/DependencyInjection/solutions/Main.java
@@ -9,12 +9,8 @@ import java.util.stream.Collectors;
 
 public final class Main {
   public static void main(String[] args) {
-    // TODO: Update this code to create a new Guice injector from the Guice module you filled in.
-    //       Use Guice.createInjector(...) to do this.
-    
-    // TODO: Instead of calling the ExpirationChecker constructor here, use the Guice injector to
-    //       get an instance of the ExpirationChecker.
-    ExpirationChecker checker = new ExpirationChecker();
+    Injector injector = Guice.createInjector(new ExpirationCheckerModule());
+    ExpirationChecker checker = injector.getInstance(ExpirationChecker.class);
 
     List<Path> paths = Arrays.stream(args).map(Path::of).collect(Collectors.toList());
 
