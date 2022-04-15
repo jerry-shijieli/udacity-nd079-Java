ba8c9a6f76db3a3e789889e43091730da8471645
Jerry <sli41@ncsu.edu>
2022-04-14 22:27:30 -0700

    Update ExpirationCheckerTest.java

--- cd0381/DependencyInjection/solutions/ExpirationCheckerTest.java
+++ cd0381/DependencyInjection/solutions/ExpirationCheckerTest.java
@@ -12,14 +12,15 @@ public final class ExpirationCheckerTest {
     Map<Path, Instant> fakeModifiedTimes = new HashMap<>();
     FakeMetadataFetcher fakeMetadataFetcher = new FakeMetadataFetcher(fakeModifiedTimes);
 
+    Injector injector = Guice.createInjector(
+        b -> b.bind(Clock.class).toInstance(fakeClock),
+        b -> b.bind(MetadataFetcher.class).toInstance(fakeMetadataFetcher));
+    ExpirationChecker checker = injector.getInstance(ExpirationChecker.class);
+
     Path expired = Path.of("/test/expired");
     Path notExpired = Path.of("/test/not-expired");
     fakeModifiedTimes.put(expired, fakeClock.instant().minus(Duration.ofDays(31)));
     fakeModifiedTimes.put(notExpired, fakeClock.instant().minus(Duration.ofDays(27)));
-
-    // TODO: Change this code to create an ExpirationChecker using the FakeClock and
-    //       FakeMetadataFetcher.
-    ExpirationChecker checker = new ExpirationChecker();
 
     List<Path> expiredFiles =
         checker.getExpiredFiles(List.of(expired, notExpired), Duration.ofDays(30));
